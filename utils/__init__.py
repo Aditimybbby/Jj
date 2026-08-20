ecbbfa0d62a8507e44cb4b99568a864393d36fdf
Aditimybbby <160321066+Aditimybbby@users.noreply.github.com>
2026-08-20 17:49:22 +0530

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,18 @@
+# This file is part of LazyFarmers.
+# Copyright (c) 2025-Present Routo
+#
+# LazyFarmers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with LazyFarmers. If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+Author: Routo
+LazyFarmers - https://github.com/routo-loop/neura-self
+"""
+
+
