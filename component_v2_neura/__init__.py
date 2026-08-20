c8d80c5854b89d81c5d2f65adb54bdda6ea32e0e
Aditimybbby <160321066+Aditimybbby@users.noreply.github.com>
2026-08-20 18:07:16 +0530

    Add files via upload

---
+++ component_v2_neura/__init__.py
@@ -0,0 +1,21 @@
+# This file is part of LazyFarmers.
+# Copyright (c) 2025-Present Routo
+#
+# LazyFarmers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with LazyFarmers. If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+Author: Routo
+LazyFarmers - https://github.com/routo-loop/neura-self
+"""
+
+
+
+from .parser import V2Component, parse_v2_message, get_boss_battle_id
+from .interactions import InteractionManager, setup_interactions
